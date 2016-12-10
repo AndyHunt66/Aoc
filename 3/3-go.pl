d25d3353cdf4a78c7ca84bdcc88b0d396f9aa32f
AndyHunt66 <andy.hunt@alfresco.com>
2016-12-10 15:17:28 +0000

    Day 3 task 2 and day 4

--- 3/3-go.pl
+++ 3/3-go.pl
@@ -1,4 +1,4 @@
-#!perl
+#!/usr/bin/perl
 
 use strict;
 use warnings;
@@ -7,18 +7,26 @@ no warnings 'experimental';
 
 sub isTriangle
 {
-	my @sides = sort(@ARGV);
+	my @sides = sort(@_);
+	my $wellIsIt = "true";
 	for (1..3)
 	{
-		$testSide = pop @sides;
-		
+		my $testSide = shift @sides;
+#		print "is testSide ... $testSide > ".int($sides[0])." + ".int($sides[1]) ." \n";
+		my $bothSides = int($sides[0])+int($sides[1]);
+#		print "Both Sides = $bothSides and testSide = $testSide \n";
+		if ($testSide >= $bothSides) { $wellIsIt= "false";}
+		push @sides, $testSide;
+		#print "BothSides = $bothSides  testSide = $testSide  Result = $wellIsIt \n";
 	}
+	print "Returning $wellIsIt \n";
+	return $wellIsIt;
 }
 
 my $star = $0;   ## Which day of the challenge it is
 my $task = 1;    ## Which part of that day's challenge - 1 or 2
 
-if ($star =~ /\\(\d+)-go.pl/)
+if ($star =~ /[\\\/](\d+)-go.pl/)
 {
 	$star=$1;
 }
@@ -36,17 +44,60 @@ open INPUT, "$inputFileName" or die "Can't open Input file ($inputFileName) for 
 print "Day ".$star."\n";
 print "Task ".$task."\n\n";
 
-## Single line reading
-my $input = <INPUT>;
-chomp $input;
+## Test
+my $testResult = isTriangle(5,10,25);
+if ($testResult eq "false") { print "Test 1 passed.\n";}
+$testResult = isTriangle(5,3,4);
+if ($testResult eq "true") { print "Test 2 passed.\n";} else {print "Test 2 failed : $testResult should be true\n"}
+$testResult = isTriangle(827,126,272);
+if ($testResult eq "false") { print "Test 3 passed.\n";} else {print "Test 3 failed : $testResult should be false\n"}
 
 
-## Multi line reading
-while (<INPUT>)
+my $falses = 0;
+my $trues = 0;
+my $total = 0;
+
+
+if ($task == 1)
 {
-	my $line = $_;
-	chomp $line;
+
+	while (<INPUT>)
+	{
+		$total++;
+		chomp;
+		my @line = split(" ",$_);
+		print "$line[0] , $line[1] , $line[2] ";
+		if ( (isTriangle($line[0],$line[1],$line[2]) eq "true")){$trues++}else{$falses++};
+	}
+	close INPUT;
+}
+else
+{
+	while (<INPUT>)
+	{
+		$total++;
+		chomp;
+		my @line1 = split(" ",$_);
+		$_ = <INPUT>;
+		$total++;
+		chomp;
+		my @line2 = split(" ",$_);
+		$_ = <INPUT>;
+		$total++;
+		chomp;
+		my @line3 = split(" ",$_);
+		print "$line1[0] , $line2[0] , $line3[0] ";
+		if ( (isTriangle($line1[0],$line2[0],$line3[0]) eq "true")){$trues++}else{$falses++};
+		print "$line1[1] , $line2[1] , $line3[1] ";
+		if ( (isTriangle($line1[1],$line2[1],$line3[1]) eq "true")){$trues++}else{$falses++};
+		print "$line1[2] , $line2[2] , $line3[2] ";
+		if ( (isTriangle($line1[2],$line2[2],$line3[2]) eq "true")){$trues++}else{$falses++};
+		
+		
+	}	
 	
 	
 }
-print "InputFileName = $inputFileName \n";
+print "Trues : $trues \n";
+print "Falses: $falses \n";
+print "Total : $total \n";
---
+++ 4/4-go.pl
@@ -0,0 +1,142 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+no warnings 'experimental';
+
+sub isRoomReal
+{
+	#print  "Room: $_[0]\n";
+	my %letters;
+	my @room = split(/[-\[\]]/,$_[0]);
+	
+	my $roomCode = pop @room; #print "RoomCode: $roomCode \n";
+	my $sectorId = pop @room; #print "SectorId : $sectorId \n";
+	
+	foreach (@room)
+	{
+		my @word = split("",$_);
+		foreach (@word)
+		{
+			$letters{$_}++;
+		}	
+	}
+	my @sortedLetters = sort 
+	{
+  	$letters{$b} <=> $letters{$a}
+    or
+  	"\L$a" cmp "\L$b"
+  } keys %letters;
+	
+	
+	my $roomValue = 0;
+	#print "Calculated roomCode= ".$sortedLetters[0].$sortedLetters[1].$sortedLetters[2].$sortedLetters[3].$sortedLetters[4]."\n";
+	if ($roomCode eq $sortedLetters[0].$sortedLetters[1].$sortedLetters[2].$sortedLetters[3].$sortedLetters[4])
+	{
+		return $sectorId;
+	}
+	return 0;
+}
+
+sub spinLetter
+{
+	my $letterToSpin = $_[0];
+	my $spinAmount = $_[1];
+	
+	for (1..$spinAmount)
+	{
+		if ($letterToSpin eq "z")
+		{
+			$letterToSpin = 'a';
+		}
+		else
+		{
+			$letterToSpin++;
+		}
+	}	
+	return $letterToSpin;
+}
+sub spinCode
+{
+	#print  "Room: $_[0]\n";
+	my $realRoomName="";
+	my @room = split(/[-\[\]]/,$_[0]);
+	
+	my $roomCode = pop @room; #print "RoomCode: $roomCode \n";
+	my $sectorId = pop @room; #print "SectorId : $sectorId \n";
+	foreach (@room)
+	{
+		if ($realRoomName ne "")
+		{
+			$realRoomName = $realRoomName." ";
+		}
+		my @word = split("",$_);
+		foreach (@word)
+		{
+			#print $_."\n";
+			$realRoomName=$realRoomName . spinLetter($_, $sectorId);
+		}	
+		$realRoomName=$realRoomName;
+	}
+	return $realRoomName;
+}
+
+my $star = $0;   ## Which day of the challenge it is
+my $task = 1;    ## Which part of that day's challenge - 1 or 2
+
+if ($star =~ /[^\d](\d+)-go.pl/)
+{
+	$star=$1;
+}
+else
+{
+	die "I don't even know what day it is... $0 \n";
+}
+if (defined $ARGV[0])
+{
+	$task=$ARGV[0];
+}	
+my $inputFileName="./".$star."-input.txt";
+open INPUT, "$inputFileName" or die "Can't open Input file ($inputFileName) for reading - $! \n";
+
+print "Day ".$star."\n";
+print "Task ".$task."\n\n";
+
+#
+if ($task eq "test")
+{
+	my $testResult=isRoomReal("aaaaa-bbb-z-y-x-123[abxyz]");
+	if ( $testResult == 123 ) { print "Test 1 passed\n";}else{print "Test 1 failed : 	$testResult should be 123 \n";}
+	
+	$testResult=isRoomReal("a-b-c-d-e-f-g-h-987[abcde]");
+	if ( $testResult == 987 ) { print "Test 2 passed\n";}else{print "Test 2 failed : 	$testResult should be 987 \n";}
+	
+	$testResult=isRoomReal("not-a-real-room-404[oarel]");
+	if ( $testResult == 404 ) { print "Test 3 passed\n";}else{print "Test 3 failed : 	$testResult should be 404 \n";}
+	
+	$testResult=isRoomReal("totally-real-room-200[decoy]");
+	if ( $testResult == 0 ) { print "Test 4 passed\n";}else{print "Test 4 failed : 	$testResult should be 0 \n";}
+	
+	$testResult=spinCode("qzmt-zixmtkozy-ivhz-343[xxxx]");
+	if ( $testResult eq "very encrypted name"){print "Test 5 passed";}else{print "Test 5 failed : $testResult should be \"very encrypted name\"\n";}
+}
+
+my $result = 0;
+while (<INPUT>)
+{
+	my $realRoomName = "";
+	my $line = $_;
+	chomp $line;
+	my $thisRoom = isRoomReal($line);
+	if ($thisRoom > 0) 
+	{
+		$result += isRoomReal($line);
+		$realRoomName = spinCode($line);
+	}
+	if ($realRoomName ne "")
+	{
+		print "Real Room: ".$realRoomName." - ". $thisRoom."\n"; ## meh - just grep the output yourself
+	}
+	
+}
+print "Result = $result \n";
