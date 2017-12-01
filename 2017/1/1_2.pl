647af600f47851c554c18bed741cfa9387c591d4
AndyHunt66 <andy.hunt@alfresco.com>
2017-12-01 16:36:04 +0000

    much simpler

--- 2017/1/1_2.pl
+++ 2017/1/1_2.pl
@@ -3,38 +3,19 @@
 use strict;
 use warnings;
 
-#my $inputFileName = "./input";
-#my $outputFileName = "./output.txt";
-
-my $cmdLineInput = $ARGV[0];
-
-#open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
-#open OUTPUTFILE, ">$outputFileName" or die "Couldn't open outputFile $outputFileName for writing - $!\n";
-
-my $input = $cmdLineInput;
+my $input = $ARGV[0];
 
 my $total = 0;
 my @numbers = split(//,$input);
 my $steps = ($#numbers+1)/2;
-print "STEPS: ". $steps."\n";
+
 for (my $count=0; $count <=$#numbers; $count++)
 {
-	print "COUNT:".$count."\n";
-		if (($count + $steps) > $#numbers-1)
-		{
-			if ($numbers[$count] == $numbers[$count-$steps])
+	#print "COUNT:".$count."\n";
+			if ($numbers[$count] == $numbers[($count+$steps)%($#numbers+1)])
 			{
 				$total = $total+$numbers[$count];
 			}
-		}
-		else
-		{
-			if ($numbers[$count] == $numbers[$count+$steps])
-			{
-				$total = $total+$numbers[$count];
-			}
-		}
-
 }
 
 print "TOTAL : " . $total."\n";
