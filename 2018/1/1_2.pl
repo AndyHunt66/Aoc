575e81693a8eeaee5664137bd344445ad198a73e
AndyHunt66 <andy.hunt@alfresco.com>
2017-12-01 14:57:54 +0000

    Initial Commit

---
+++ 2018/1/1_2.pl
@@ -0,0 +1,40 @@
+﻿#!/bin/perl
+
+use strict;
+use warnings;
+
+#my $inputFileName = "./input";
+#my $outputFileName = "./output.txt";
+
+my $cmdLineInput = $ARGV[0];
+
+#open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
+#open OUTPUTFILE, ">$outputFileName" or die "Couldn't open outputFile $outputFileName for writing - $!\n";
+
+my $input = $cmdLineInput;
+
+my $total = 0;
+my @numbers = split(//,$input);
+my $steps = ($#numbers+1)/2;
+print "STEPS: ". $steps."\n";
+for (my $count=0; $count <=$#numbers; $count++)
+{
+	print "COUNT:".$count."\n";
+		if (($count + $steps) > $#numbers-1)
+		{
+			if ($numbers[$count] == $numbers[$count-$steps])
+			{
+				$total = $total+$numbers[$count];
+			}
+		}
+		else
+		{
+			if ($numbers[$count] == $numbers[$count+$steps])
+			{
+				$total = $total+$numbers[$count];
+			}
+		}
+
+}
+
+print "TOTAL : " . $total."\n";
