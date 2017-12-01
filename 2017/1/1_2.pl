81f05a5d807f6d0065983d4dce29a4232007dd15
AndyHunt66 <andy.hunt@alfresco.com>
2017-12-01 19:34:41 +0000

    Moar Spee3d

--- 2017/1/1_2.pl
+++ 2017/1/1_2.pl
@@ -7,10 +7,9 @@ my $total = 0;
 my @numbers = split(//,$ARGV[0]);
 my $steps = ($#numbers+1)/2;
 
-for (my $count=0; $count <=$#numbers; $count++)
+for (my $count=0; $count <=$steps $count++)
 {
-	#print "COUNT:".$count."\n";
-			if ($numbers[$count] == $numbers[($count+$steps)%($#numbers+1)])
+			if ($numbers[$count] == $numbers[($count+$steps)])
 			{
 				$total = $total+$numbers[$count];
 			}
