c1f5a68a2002889efce5d4fdc8fbaf1c92e89742
AndyHunt66 <andy.hunt@alfresco.com>
2017-12-02 10:17:32 +0000

    2015 day 1/2

---
+++ 2015/2/2_2.pl
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+
+my $inputFileName = "./input.txt";
+
+open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
+
+my $total=0;
+
+while (<INPUTFILE>)
+{
+	my @sides=sort {$a <=> $b} split(/x/,$_);
+	$total = $total + ( (2 *  $sides[0]) + (2*$sides[1])  + ($sides[0]*$sides[1]*$sides[2]) )
+}
+print "TOTAL: ".($total)."\n";
+
