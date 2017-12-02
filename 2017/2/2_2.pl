de9c14d3d2bb86fa6e8c7b196c7ff16463671adf
AndyHunt66 <andy.hunt@alfresco.com>
2017-12-02 09:43:41 +0000

    Day 2 part 2

---
+++ 2017/2/2_2.pl
@@ -0,0 +1,32 @@
+﻿#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+
+my $inputFileName = "./input.txt";
+
+open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
+
+my $total=0;
+
+while (<INPUTFILE>)
+{
+	my @numbers=sort {$a <=> $b} split(/[ \t]/,$_);
+	
+	for(my $count=$#numbers; $count>=0;$count--)
+	{
+		my $dividend = $numbers[$count];
+		for (my $innerCount = 0; $innerCount < $count; $innerCount++)
+		{
+			my $divisor = $numbers[$innerCount];
+			if (($dividend % $divisor)==0)
+			{
+				$total = $total + ($dividend/$divisor);
+				last;
+			}
+		}
+	}
+}
+print "TOTAL: ".$total."\n";
+
