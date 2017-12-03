c670c4e706f8729e2f83c826980a9e7f7d9d7176
Andy Hunt <andy.hunt@alfresco.com>
2017-12-03 11:29:38 +0000

    day 3 part 1

--- 2017/3/3_1.pl
+++ 2017/3/3_1.pl
@@ -3,7 +3,28 @@
 use strict;
 use warnings;
 
-my $inputFileName = "./input.txt";
+# First Part
+# sqrt(265149)=514.92
+# 514*514=264196
+# 265149-264196=953
+# 953-514=439
+# 439-(514/2)=182
+# (514+182)/2=348 ##wrong
+# 264712 +(514/2)=  264969
+# 265149-264969=180 ## 180 HORIZONTAL MOVES
+# (514/2)= 257 vertical moves
+# 257+180+1=438
+
+###
+#
+#   264197  264196
+#   .   
+#.
+#
+#   .
+#   
+#   264712.......     264969                 265227
+
 
 open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
 
