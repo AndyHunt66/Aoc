1e321b42063560ce0641d084d8ab6e370c0d66db
AndyHunt66 <andy.hunt@alfresco.com>
2017-12-05 09:26:37 +0000

    Day 5

---
+++ 2017/5/12_1.pl
@@ -0,0 +1,49 @@
+﻿#!/bin/perl
+
+use strict;
+use warnings;
+
+my $inputFileName = "./input.txt";
+
+open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
+
+my @memArray;
+my $listSize=0;
+my $count=0;
+while (<INPUTFILE>)
+{
+	chomp;
+	@memArray[$count]=$_;
+	$count++;
+}
+$listSize=$count;
+print "Size of grid = ".$count ."\n";
+<>;
+my $currentPosition = 0;
+my $numSteps=0;
+while ($currentPosition <$listSize)
+{
+	$numSteps++;
+	print "Move #".$numSteps;
+	$currentPosition = &move($currentPosition);
+	if ($currentPosition > $listSize)
+	{
+		last;
+	}
+}
+
+print "STEPS TAKEN: ".$numSteps."\n";
+
+
+
+sub move
+{
+	my $start=$_[0];
+	my $stepsToTake=$memArray[$start];
+	$memArray[$start]=$memArray[$start]+1;
+	print " Starting Point:".$start."   Steps:".$stepsToTake."\n";
+	return $start + $stepsToTake;
+	
+}
+
+# Grid Size : 1052
---
+++ 2017/5/12_2.pl
@@ -0,0 +1,60 @@
+﻿#!/bin/perl
+
+use strict;
+use warnings;
+
+my $inputFileName = "./input.txt";
+#my $inputFileName = "./inputTest.txt";
+
+open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
+
+print "MOD:".(619387 % 100000)."<---\n";
+my @memArray;
+my $listSize=0;
+my $count=0;
+while (<INPUTFILE>)
+{
+chomp;
+@memArray[$count]=$_;
+$count++;
+}
+$listSize=$count;
+print "Size of grid = ".$count ."\n";
+<>;
+my $currentPosition = 0;
+my $numSteps=0;
+while ($currentPosition <$listSize)
+{
+$numSteps++;
+#print "Move #".$numSteps;
+$currentPosition = &move($currentPosition);
+if ($currentPosition > $listSize)
+{
+last;
+}
+}
+
+print "STEPS TAKEN: ".$numSteps."\n";
+
+
+
+sub move
+{
+	my $start=$_[0];
+	my $stepsToTake=$memArray[$start];
+	if ($memArray[$start]>= 3) 
+	{
+		$memArray[$start]=$memArray[$start]-1;
+	}
+	else
+	{
+		$memArray[$start]=$memArray[$start]+1;
+	}
+
+	if (($numSteps % 100000) == 0)
+	{print "Move #:".$numSteps." Starting Point:".$start."   Steps:".$stepsToTake."\n"}
+	return $start + $stepsToTake;
+
+}
+
+# Grid Size : 1052
