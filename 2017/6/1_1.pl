84362c3e6c07833a8b3e12f6febb94923db25424
Andy Hunt <andy.hunt@alfresco.com>
2017-12-06 05:51:21 +0000

    6

---
+++ 2017/6/1_1.pl
@@ -0,0 +1,33 @@
+﻿#!/bin/perl
+
+use strict;
+use warnings;
+
+my $inputFileName = "./input.txt";
+
+open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
+
+my %splits;
+
+my @banks;
+
+while (<INPUTFILE>)
+{
+  @banks = split(//,$_);
+}
+close INPUTFILE;
+
+while(1)
+{
+  $biggest=7;
+  foreach (@banks)
+{
+
+
+}
+
+
+}
+die;
+
+
