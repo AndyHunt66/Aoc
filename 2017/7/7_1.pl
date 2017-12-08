d7a4ff8517d098244db964a54d55441b89ae1b1b
AndyHunt66 <andy.hunt@alfresco.com>
2017-12-08 15:36:37 +0000

    Removed rubbish

--- 2017/7/7_1.pl
+++ 2017/7/7_1.pl
@@ -22,13 +22,6 @@ while (<INPUTFILE>)
 }
 close INPUTFILE;
 
-#print "NODE: ".$nodes{fwft}[0]."\n";
-#print "NODE: ".$nodes{fwft}[1]."\n";
-#print "NODE: ".$nodes{fwft}[2]."\n";
-#print "NODE: ".$nodes{fwft}[3]."\n";
-#print "NODE: ".$nodes{fwft}[4]."\n";
-
-## Set levels
 	my %parents= %nodes;
 	my $level=0;
 	my @levels;
@@ -39,20 +32,10 @@ while ((scalar keys %parents) > 0)
 	print "LEVEL:".$level."\n";
 	#<>;
 	my @thisLevel;
-	foreach ( keys %parents)
+	foreach my $node ( keys %parents)
 	{
-		my $node=$_;
-		#print "PARENT Node:".$node."\n";
-		#foreach($nodes{$node}){print "   ".$_."\n";}
-		#print "   ".$parents{$node}[0]."\n";
-		#print "   ".$parents{$node}[1]."\n";
-		#print "   ".$parents{$node}[2]."\n";
-		#print "   ".$parents{$node}[3]."\n";
-		#print "   ".$parents{$node}[4]."\n";
-		#print "   ".$nodes{$node}[5]."\n";
 		if (!$parents{$node}[2])
 		{
-			#print "DELETING Child ".$node." from parents\n";
 			##  No more children
 			$nodes{$node}[0]=$level;
 			push @thisLevel, $node;
@@ -65,38 +48,22 @@ while ((scalar keys %parents) > 0)
 	print "Number in level $level ".@thisLevel."\n";
 	foreach my $thisChild (@thisLevel)
 	{
-		#foreach (@thisLevel) {print "  ".$_."\n";}
-		#print "This Child: ".$thisChild."\n";
 		my $found=0;
 		foreach my $thisParent ( keys %parents)
 		{
-			#print "This parent: ".$thisParent."\n";#" -- ".$parents{$thisParent}."\n";# " -- ".$parents{$thisParent}[0]." -- ".$parents{$thisParent}[1]." -- ".$parents{$thisParent}[2]."\n";
-			
-			my @children;
-			foreach(@{$parents{$thisParent}})
-			{
-				#print "Pushing ".$_."\n";
-				push @children, $_;
-			}
-			#my @children=@{$parents{$thisParent}};
-			
-			#print "Number of children of this parent: " .scalar @children."\n";
+			my @children =@{$parents{$thisParent}};
 
 			for (my $childNum=2;$childNum<=$#children;$childNum++)
 			{
-				#print "Comparing ".$thisChild ." with ".$children[$childNum]."\n";
 				if ($thisChild eq $children[$childNum])
 				{
 					$found=1;
-					#print "Now Parent $thisParent has ".$#children." children.\n";
 					splice @children,$childNum,1;
 					$parents{$thisParent}=\@children;
-					#print "Now Parent $thisParent only has ".$#children." children.".scalar $parents{$thisParent}."\n";
 					last;
 				}
 			}
 			if ($found == 1){ last}
-			
 		}
 	}
 	$level++;
@@ -109,5 +76,4 @@ for (my $count=0; $count<=$#levels;$count++)
 	{
 		print "  ".$_."\n";
 	}
-	
 }
