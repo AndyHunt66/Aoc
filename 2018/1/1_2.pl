a5fcd3a94f8c8d3918b032ad2dcabccaaabea638
Andy Hunt <andy.hunt@alfresco.com>
2018-12-02 09:59:46 +0000

    First Two days of 2018

---
+++ 2018/1/1_2.pl
@@ -0,0 +1,43 @@
+﻿#!/bin/perl
+
+use strict;
+use warnings;
+
+my $inputFileName = "./input.txt";
+my $outputFileName = "./output.txt";
+
+my $cmdLineInput = $ARGV[0];
+
+open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
+#open OUTPUTFILE, ">$outputFileName" or die "Couldn't open outputFile $outputFileName for writing - $!\n";
+
+my $freq = 0;
+my %freqs ;
+my $found = 0;
+while ($found == 0)
+{
+    while (<INPUTFILE>)
+    {
+        /^(\+|-)(\d*)$/;
+        if ($1 eq "+")
+        {
+            $freq += $2;
+        }
+        else
+        {
+            $freq -= $2;
+        }
+        if (defined $freqs{$freq})
+        {
+            $found = 1;
+            last;
+        }
+        else
+        {
+            $freqs{$freq}=1;
+        }
+    }
+    close INPUTFILE;
+    open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
+}
+print "Final Frequency= " . $freq;
