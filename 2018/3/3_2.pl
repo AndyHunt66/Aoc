17d715a7285efd9b90c1812f4dd281314f3bf4e2
Andy Hunt <andy.hunt@alfresco.com>
2018-12-03 23:13:08 +0000

    Day 3 part 2

---
+++ 2018/3/3_2.pl
@@ -0,0 +1,66 @@
+﻿#!/bin/perl
+
+use strict;
+use warnings;
+
+my $inputFileName = "./input.txt";
+
+open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
+
+my @fabric;
+for my $i (0..1000)
+{
+    for my $j (0..1000)
+    {
+        $fabric[$i][$j]=0;
+    }
+}
+
+
+while (<INPUTFILE>)
+{
+
+    # #1 @ 37,526: 17x23
+    my $line = $_;
+    my @claim = split(/[ ,:x]/,$line);
+   # print "Top left corner = ".$claim[2]." - ".$claim[3]." -- Size : ".$claim[5]." by ".$claim[6]."\n";
+
+
+    for my $row ($claim[2]..($claim[2]+$claim[5]-1))
+    {
+        for my $col ($claim[3]..($claim[3]+$claim[6]-1))
+        {
+            $fabric[$row][$col]++;
+        }
+    }
+}
+close INPUTFILE;
+open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
+
+while (<INPUTFILE>)
+{
+    my $clear=1;
+    # #1 @ 37,526: 17x23
+    my $line = $_;
+    my @claim = split(/[ ,:x]/,$line);
+    # print "Top left corner = ".$claim[2]." - ".$claim[3]." -- Size : ".$claim[5]." by ".$claim[6]."\n";
+
+
+    for my $row ($claim[2]..($claim[2]+$claim[5]-1))
+    {
+        for my $col ($claim[3]..($claim[3]+$claim[6]-1))
+        {
+            if ($fabric[$row][$col]>1)
+            {
+                $clear=0;
+                next;
+            }
+        }
+        if ($clear==0){next;}
+    }
+    if ($clear==1)
+    {
+        print "Found ID ".$claim[0]."\n";
+        die;
+    }
+}
