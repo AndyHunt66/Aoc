ce2b3638d3032bc0d348ac4312ffb413f72c890d
Andy Hunt <andy.hunt@alfresco.com>
2018-12-07 21:56:13 +0000

    First half of Day 5

---
+++ 2018/5/5_1.pl
@@ -0,0 +1,49 @@
+﻿#!/bin/perl
+
+use strict;
+use warnings;
+
+my $inputFileName = "./input.txt";
+my $chunksize = 10;
+
+open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
+
+
+my $full = <INPUTFILE>;
+chomp($full);
+my @full = split //,$full;
+my $combined = 0;
+while (1)  
+{
+	for (my $count = 0;$count < $#full;)
+	{
+	#	for(0..5)
+	#	{
+	#		print $full[$count+$_];
+	#	}
+	#	print "\n";#<>;
+		if (($full[$count] =~ /$full[$count+1]/i) &&
+		     ($full[$count] !~ /$full[$count+1]/))
+		{
+			splice(@full,$count,2);	 
+			$combined++;
+		}
+		else
+		{
+			$count++;
+		}
+	}
+	print "Finished pass - $combined combined\n";
+	if ($combined == 0)
+	{
+		print "Final string: ";
+		foreach (@full)
+		{
+			print;
+		}
+		print "\n";
+		print "Size: ". scalar @full."\n";
+		die;
+	}
+	$combined = 0;
+}
--- 2018/template.pl
+++ 2018/template.pl
@@ -4,12 +4,10 @@ use strict;
 use warnings;
 
 my $inputFileName = "./input.txt";
-my $outputFileName = "./output.txt";
 
-my $cmdLineInput = $ARGV[0];
 
 open INPUTFILE, "$inputFileName" or die "Couldn't open InputFile $inputFileName for reading - $!\n";
-open OUTPUTFILE, ">$outputFileName" or die "Couldn't open outputFile $outputFileName for writing - $!\n";
+
 
 
 
