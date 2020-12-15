4964ffef6919b4afaf9bc9fd1fda6617ceb603c0
Andy Hunt <andrew.hunt@elastic.co>
2020-12-15 07:20:48 +0000

    Day 15

---
+++ 2020/15/part1.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#my $input = './testinput.txt'; 
+#my $input = './testinput2.txt';
+my $input = './input';
+
+my $MAXTURN = 30000000;
+my @entries;
+my $inputs = "";
+open INPUTFILE, "$input" or die "Can't open input file $input for reading - $! \n";
+chomp(@entries = <INPUTFILE>);
+close INPUTFILE;
+
+@entries = split /,/ , $entries[0];
+
+print $entries[0]."\n";
+print $entries[1]."\n";
+print $entries[2]."\n";
+
+my %track;
+my $turn = 0;
+my $nextNumber = 0;
+my $thisNumber = 0;
+# Read the numbers on the paper
+foreach (@entries)
+{
+    $turn++;
+    $track{$_}=$turn;
+}
+
+## Start the game
+while (1)
+{
+    $turn++;
+    $thisNumber = $nextNumber;                                 
+    #print $turn." " . $thisNumber."\n";                    
+    if ($turn == $MAXTURN)
+    {
+        print "Final number: ".$thisNumber."\n";
+        die;
+    }
+   if (defined $track{$thisNumber})                          
+    { 
+        $nextNumber = ($turn  ) - $track{$thisNumber};    
+    }
+    else 
+    {
+        $nextNumber = 0;
+    }
+    $track{$thisNumber}=$turn;
+}
+    
