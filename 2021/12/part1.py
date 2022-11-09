78e171e049fabd2670ecef7c8ffa3a80482e77a3
Andy Hunt <andy.hunt@dremio.com>
2022-11-09 17:55:59 +0000

    Complete Day 12 parts 1 and 2 in the same file

--- 2021/12/part1.py
+++ 2021/12/part1.py
@@ -1,74 +1,98 @@
+from iteration_utilities import duplicates
 import copy
 import sys
 import re
+import networkx as nx
 
-# inFile = './testInput.txt'
+
+
+def getnodesfromnode(node):
+    pathsfromnode = []
+    for path in cavemap:
+        if path[0] == node:
+            pathsfromnode.append(path[1])
+        if path[1] == node:
+            pathsfromnode.append(path[0])
+    return pathsfromnode
+
+def findpath(paths):
+    for thispath in paths:
+        if thispath[-1] == "end":
+            continue
+        possiblenodes = getnodesfromnode(thispath[-1])
+        validpaths = []
+
+        ## remove any paths that lead to small caves already visited
+        for nextnode in possiblenodes:
+
+            ## Don't go back to the start
+            if nextnode == "start":
+                continue
+            ## Any path from here to a large (upper case) cave is valid
+            if nextnode.isupper():
+                validpaths.append(nextnode)
+                continue
+            ## Only add this path if nextnode doesn't already exist in path
+            valid = True
+
+            ## for Part 2
+            doubleexists = False
+            thispathlower = []
+            for node in thispath:
+                if node != "start" and node != "end" and node.islower():
+                    if node in thispathlower:
+                        doubleexists = True
+                        break
+                    thispathlower.append(node)
+
+            for node in thispath:
+                if PART == 1:
+                    if nextnode == node:
+                        valid = False
+                        break
+                else: #PART = 2
+                    if nextnode == node and doubleexists:
+                        valid = False
+                        break
+            if valid:
+                validpaths.append(nextnode)
+        if len(validpaths) == 0:
+            paths.remove(thispath)
+        for x in range( len(validpaths)-1, -1,-1 ):
+            if x == 0:
+                thispath.append(validpaths[x])
+                continue
+            newpath = copy.deepcopy(thispath)
+            newpath.append(validpaths[x])
+            paths.append(newpath)
+
+    for thispath in paths:
+            if thispath[-1] != "end":
+                for path in paths:
+                    print(path)
+                print("===========================")
+                findpath(paths)
+                break
+
+## Are we running Part 1 or Part 2 of the puzzle?
+PART=2
+
+#inFile = './testInput3.txt'
 inFile = './input.txt'
 
-def runOneStep(octopi):
-    numFlashes = 0
-    for row in range(len(octopi)):
-        for octopus in range(len(octopi[row])):
-            octopi[row][octopus] += 1
-    done = False
-    while done is False:
-        done = True
-        for row in range(len(octopi)):
-            for octopus in range(len(octopi[row])):
-                if octopi[row][octopus] > 9:
-                    done = False
-                    # flash()
-                    # Top Row
-                    if row - 1 >= 0:
-                        if octopus - 1 >= 0 and octopi[row-1][octopus-1] != 0:
-                            octopi[row-1][octopus-1] += 1
-                        if octopi[row-1][octopus] != 0:
-                            octopi[row-1][octopus] += 1
-                        if octopus + 1 < len(octopi[row]) and octopi[row-1][octopus+1] != 0:
-                            octopi[row-1][octopus+1] += 1
-                    # Middle Row
-                    if octopus - 1 >= 0 and octopi[row][octopus-1] != 0:
-                        octopi[row][octopus-1] += 1
-                    octopi[row][octopus] = 0
-                    numFlashes += 1
-                    if octopus + 1 < len(octopi[row]) and octopi[row][octopus+1] != 0:
-                        octopi[row][octopus+1] += 1
-                    # Bottom Row
-                    if row + 1 < len(octopi):
-                        if octopus - 1 >= 0 and octopi[row+1][octopus-1] != 0:
-                            octopi[row+1][octopus-1] += 1
-                        if octopi[row+1][octopus] != 0:
-                            octopi[row+1][octopus] += 1
-                        if octopus + 1 < len(octopi[row]) and octopi[row+1][octopus+1] != 0:
-                            octopi[row+1][octopus+1] += 1
-    return numFlashes
-
-octopi = []
+cavemap  = []
 with open(inFile, 'r') as fh:
     for line in fh:
-        octopi.append([int(x) for x in str(line.strip())])
+        cavemap.append(line.strip().split("-"))
 
-numFlashes = 0
-firstFullFlash = 0
-for i in range(1000):
-    newFlashes = runOneStep(octopi)
-    print("New Flashes: ", newFlashes)
-    numFlashes += newFlashes
-    if newFlashes == len(octopi) * len(octopi[0]):
-        firstFullFlash = i
-        print("First Full Flash : ", i+1)
-        exit()
-    # for row in octopi:
-    #     for octopus in row:
-    #         print(octopus, end="")
-    #     print()
-    # print("-------------------------")
+print(cavemap)
+paths = []
+path = []
+path.append("start")
+paths.append(path)
 
-print("number of Flashes : ", numFlashes)
-# for row in octopi:
-#     for octopus in row:
-#         print(octopus, end="")
-#     print()
-
-
-
+findpath(paths)
+for path in paths:
+    print(path)
+print("===========================")
+print("Number of paths: ", len(paths))
