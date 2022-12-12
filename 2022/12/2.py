9f8bd64c0ebcb7cd6fddbc8b8d3edbf3117bd2bd
Andy Hunt <andy.hunt@dremio.com>
2022-12-12 11:23:03 +0000

    Day 12 Horrible nasty part 2

---
+++ 2022/12/2.py
@@ -0,0 +1,99 @@
+import sys
+
+
+# inFile = './testInput.txt'
+inFile = './input.txt'
+
+
+def checkNextMoves(startingpoint):
+
+    endpoint = flatCavern.index("E")
+    routeCosts[startingpoint] = 0
+    while startingpoint < len(flatCavern):
+        # print(startingpoint)
+
+        if flatCavern[startingpoint] == "E" or routeCosts[startingpoint] == sys.maxsize:
+            startingpoint += 1
+            continue
+        if flatCavern[startingpoint] == "S":
+            currentHeight = "a"
+        else:
+            currentHeight = flatCavern[startingpoint]
+
+        ## Ugh... horrible
+        flatCavern[endpoint] = "z"
+
+        rightMove = startingpoint + 1
+        downMove = startingpoint + cavernWidth
+        leftMove = startingpoint - 1
+        upMove = startingpoint - cavernWidth
+
+        if (rightMove % cavernWidth) != 0 and (rightMove <= len(flatCavern)-1):
+            if ord(flatCavern[rightMove]) <= ord(currentHeight)+1:
+                routeCosts[rightMove] = min(routeCosts[startingpoint] + 1, routeCosts[rightMove])
+
+        if downMove <= len(flatCavern)-1:
+            if ord(flatCavern[downMove]) <= ord(currentHeight)+1:
+                routeCosts[downMove] = min(routeCosts[startingpoint] + 1, routeCosts[downMove])
+
+        if (startingpoint % cavernWidth) != 0 and leftMove > 0:
+            if ord(flatCavern[leftMove]) <= ord(currentHeight)+1:
+                if (routeCosts[startingpoint] + 1) < routeCosts[leftMove]:
+                    # print("Uhoh - Left Move from need to do some more work here" , startingpoint)
+                    # print("   previous cost: ", routeCosts[leftMove])
+                    # print("   new cost     : ", routeCosts[startingpoint] + flatCavern[leftMove])
+                    routeCosts[leftMove] = routeCosts[startingpoint] + 1
+                    startingpoint = leftMove
+                    continue
+
+        if upMove > 0:
+            if ord(flatCavern[upMove]) <= ord(currentHeight)+1:
+                if (routeCosts[startingpoint] + 1) < routeCosts[upMove]:
+                    # print("Uhoh - Up Move from need to do some more work here" , startingpoint)
+                    routeCosts[upMove] = routeCosts[startingpoint] + 1
+                    startingpoint = upMove
+                    continue
+
+        ## Ugh... horrible
+        flatCavern[endpoint] = "E"
+
+        startingpoint += 1
+
+
+cavern = []
+flatCavern = []
+routeCosts = []
+cavernWidth = 0
+textCavern = ''
+
+with open(inFile, 'r') as fh:
+    for line in fh:
+        if line == "\n":
+            continue
+        textCavern += line.strip()
+        cavern.append(list(line.strip()))
+        cavernWidth = len(line.strip())
+        routeCosts = routeCosts + [sys.maxsize] * cavernWidth
+flatCavern = list(textCavern)
+routeCosts[0] = 0
+
+# print ("CavernWidth: ", cavernWidth)
+# for row in cavern:
+#     print(row)
+# print(flatCavern)
+# exit()
+currentPoint = 0
+
+checkNextMoves(flatCavern.index("S"))
+print("Position of End: {0}".format([flatCavern.index("E")]))
+print("Width:", cavernWidth)
+print("Part 1 Answer is ", routeCosts[flatCavern.index("E")])
+
+ACost = []
+for pos, x in enumerate(flatCavern):
+    print("Position: ", pos)
+    if x == "a":
+        routeCosts = [sys.maxsize] * len(flatCavern)
+        checkNextMoves(pos)
+        ACost.append(routeCosts[flatCavern.index("E")])
+print("Min A cost: ", min(ACost))
