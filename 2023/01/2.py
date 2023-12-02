c515c2e0abf52a04e392ffcfb6dfae96309b1a5d
andyhunt66 <andy@andrewhunt.me.uk>
2023-12-02 18:26:22 +0000

    Day one, part two - MOAR comedy solution.....

--- 2023/01/2.py
+++ 2023/01/2.py
@@ -3,23 +3,21 @@ import re
 # inFile = './testInput2.txt'
 inFile = './input.txt'
 
-badges = []
 f = open(inFile, 'r')
 input = [line.strip() for line in f.readlines()]
 
-numbers = ['one','two', 'three','four','five','six','seven','eight','nine']
-answer=0
+answer = 0
 for cv in input:
-    cv = re.sub('one', 'one1one',cv)
-    cv = re.sub('two', 'two2two', cv)
-    cv = re.sub('three', 'three3three', cv)
-    cv = re.sub('four', 'four4four',cv)
-    cv = re.sub('five', 'five5five',cv)
-    cv = re.sub('six', 'six6six',cv)
-    cv = re.sub('seven', 'seven7seven',cv)
-    cv = re.sub('eight', 'eight8eight',cv)
-    cv = re.sub('nine', 'nine9nine',cv)
-    value = (10*int(cv[re.search('\d', cv).start()])) +  int(cv[::-1][re.search('\d',  cv[::-1]).start()])
+    cv = re.sub('eight', 'e8t', cv)
+    cv = re.sub('nine', 'n9', cv)
+    cv = re.sub('one', 'o1', cv)
+    cv = re.sub('two', '2', cv)
+    cv = re.sub('three', '3', cv)
+    cv = re.sub('four', '4', cv)
+    cv = re.sub('five', '5', cv)
+    cv = re.sub('six', '6', cv)
+    cv = re.sub('seven', '7', cv)
+    value = (10 * int(cv[re.search('\d', cv).start()])) + int(cv[::-1][re.search('\d', cv[::-1]).start()])
     print(value)
     answer += value
 
