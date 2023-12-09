541447936453e04e491d4bf3a8a0aae284b7555c
andyhunt66 <andy@andrewhunt.me.uk>
2023-12-09 12:09:55 +0000

    Day 9 - This is all my own code, but I needed a little help (since deleted) from Substantial_Sign_827 https://www.reddit.com/r/adventofcode/comments/18e5ytd/comment/kclmyaa

---
+++ 2023/09/1.py
@@ -0,0 +1,23 @@
+# https://adventofcode.com/2023/day/9
+# inFile = './testInput.txt'
+inFile = './input.txt'
+
+seeds = [[int(x) for x in line.strip().split(' ')] for line in open(inFile, 'r').readlines()]
+added=[]
+
+for idx1, seed in enumerate(seeds):
+    newseed = []
+    found = False
+    while not found:
+        newseed.append([see - seed[idx-1] for idx, see in enumerate(seed) if idx != 0 ])
+        if all(s == 0 for s in newseed[-1]) :
+
+            found = True
+            newseed[-1].append(0)
+            while len(newseed) > 1:
+                newseed[-2].append(newseed[-2][-1]+newseed[-1][-1])
+                newseed.pop(-1)
+            added.append(seeds[idx1][-1] + newseed[-1][-1])
+        seed=newseed[-1]
+print(sum(added))
+
