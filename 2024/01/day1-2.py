3def75c044fd19d3a5f329afa032e91007f2f6ad
andyhunt66 <andy@andrewhunt.me.uk>
2024-12-01 11:47:08 +0000

    2024 day 1, part 2

--- 2024/01/day1-2.py
+++ 2024/01/day1-2.py
@@ -7,12 +7,16 @@ f = open(inFile, 'r')
 input = [line.strip() for line in f.readlines()]
 
 list1 = sorted([i.split(' ',1)[0] for i in input])
-list2 = sorted([i.split('  ',1)[1] for i in input])
-print(list1)
-print(list2)
-sim1 = defaultdict(lambda: 0)
-sim2 = defaultdict(lambda: 0)
-for i in list1:
-    sim1["i"]=sim1["i"]+1
+list2 = sorted([i.split('  ',1)[1].strip() for i in input])
+# print(list1)
+# print(list2)
 
-print(sim1)
+sim2 = defaultdict(lambda: 0)
+for i in list2:
+    sim2[i]=sim2[i]+1
+
+similarity = 0
+for i in list1:
+    similarity = similarity + (int(i) * sim2[i])
+
+print(similarity)
