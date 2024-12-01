4827c8296c71f6c50457966b815c650a2a568cac
andyhunt66 <andy@andrewhunt.me.uk>
2024-12-01 11:57:04 +0000

    2024 day 1, part 2, better

--- 2024/01/day1-2.py
+++ 2024/01/day1-2.py
@@ -4,19 +4,15 @@ from collections import defaultdict
 inFile = './input.txt'
 
 f = open(inFile, 'r')
-input = [line.strip() for line in f.readlines()]
+inputValues = [line.strip() for line in f.readlines()]
 
-list1 = sorted([i.split(' ',1)[0] for i in input])
-list2 = sorted([i.split('  ',1)[1].strip() for i in input])
-# print(list1)
-# print(list2)
+list1 = sorted([i.split(' ', 1)[0] for i in inputValues])
+list2 = sorted([i.split(' ', 1)[1].strip() for i in inputValues])
 
 sim2 = defaultdict(lambda: 0)
 for i in list2:
-    sim2[i]=sim2[i]+1
+    sim2[i] = sim2[i]+1
 
-similarity = 0
-for i in list1:
-    similarity = similarity + (int(i) * sim2[i])
+similarity = sum(int(i) * sim2[i] for i in list1)
 
 print(similarity)
