6093381316689ea48252768e43b8a44689c27457
andyhunt66 <andy@andrewhunt.me.uk>
2024-12-03 08:44:00 +0000

    Day 3 done

--- 2024/03/day3-1.py
+++ 2024/03/day3-1.py
@@ -5,25 +5,10 @@ inFile = './input.txt'
 
 
 f = open(inFile, 'r')
-input =  f.read()
+input = f.read()
 
 # input = testInput
-operations = [operation.split(',') for operation in  re.findall("mul\((\d+,\d+)\)",input)]
+operations = [operation.split(',') for operation in re.findall("mul\((\d+,\d+)\)",input)]
 total = sum([int(a) * int(b) for a,b in operations ])
 print(operations)
 print(total)
-
-input2 = "do()"+"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"+"don't()"
-input2 = "do()"+input+"don't()"
-# print(input2)
-
-
-
-operations2 =  [operation.split(',') for operation in re.findall("do\(\)(.*?)don't\(\)",input2)]
-print(operations2)
-runningTotal = 0
-for text in operations2:
-    operations = [operation.split(',') for operation in  re.findall("mul\((\d+,\d+)\)",text)]
-    runningTotal = runningTotal + sum([int(a) * int(b) for a,b in operations ])
-
-print(runningTotal)
