91465a98863127954c199740a6a8788b3187f749
andyhunt66 <andy@andrewhunt.me.uk>
2024-12-14 08:19:30 +0000

    Day 14 part 2

--- 2024/14/Day14-1.py
+++ 2024/14/Day14-1.py
@@ -1,29 +1,29 @@
 import re
 
-inFile = './testInput.txt'
+# inFile = './testInput.txt'
 inFile = './input.txt'
 
 seconds = 100
 # testInput
-p1FieldX = 11
-p1FieldY = 7
+# p1FieldX = 11
+# p1FieldY = 7
 # Actual Input
 p1FieldX = 101
 p1FieldY = 103
 
 # LU,RU,LL,RL
-quads = [0,0,0,0,]
+quads = [0, 0, 0, 0]
 
 f = open(inFile, 'r')
 
 for robot in f.readlines():
-    x,y,dx,dy = map(int, re.findall(r'-?\d+',robot))
+    x, y, dx, dy = map(int, re.findall(r'-?\d+', robot))
 
     endPos = 0
-    endX = (x + ( dx * seconds)) % p1FieldX
-    endY = (y + ( dy * seconds)) % p1FieldY
+    endX = (x + (dx * seconds)) % p1FieldX
+    endY = (y + (dy * seconds)) % p1FieldY
 
-    if endX + 1== (p1FieldX + 1) / 2 or endY + 1 == (p1FieldY + 1) / 2:
+    if endX + 1 == (p1FieldX + 1) / 2 or endY + 1 == (p1FieldY + 1) / 2:
         continue
     if endX > p1FieldX / 2:
         endPos = endPos + 1
